dfbf5495a470b547cd012a53a18ba56bb60681e3
Vitor de Souza <vitordesouza@hotmail.com.br>
2022-01-17 22:44:50 -0500

    Revert "Typescript (#1)" (#2)
    This reverts commit 37086aae114843672fc0557fd37fa3d5b77f2946.

--- src/monica/definitions/IProfile.d.ts
+++
@@ -1,7 +0,0 @@
-import { IGameStage } from "/monica/definitions/IGameStage.js"
-
-export interface IProfile {
-    get Stage(): IGameStage | undefined;
-    get CanBeExecuted(): boolean;
-    Execute(): Promise<boolean>;
-}
