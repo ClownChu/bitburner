68882ad75538664aefb9d0dfb15cf187e10989e4
Vitor de Souza <vitordesouza@hotmail.com.br>
2022-01-17 22:44:27 -0500

    Revert "Typescript (#1)"
    This reverts commit 37086aae114843672fc0557fd37fa3d5b77f2946.

--- src/monica/definitions/IGameAction.d.ts
+++
@@ -1,7 +0,0 @@
-import { GameCondition } from "/monica/conditions/GameCondition.js"
-
-export interface IGameAction {
-    Id: string;
-    Conditions: GameCondition[];
-    Execute(): Promise<boolean>;
-}
--- src/monica/definitions/IGameStage.d.ts
+++
@@ -1,9 +0,0 @@
-import { IGameAction } from "/monica/definitions/IGameAction.js"
-import { GameCondition } from "/monica/conditions/GameCondition.js"
-
-export interface IGameStage {
-    Id: string;
-    Objective: string;
-    Coniditions: GameCondition[];
-    Actions: IGameAction[];
-}
